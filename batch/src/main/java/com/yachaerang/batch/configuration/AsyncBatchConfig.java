bad980bbcef03595893d39334ed33c7568c2260a
yurry45 <yurry45@naver.com>
2026-03-20 21:52:11 +0900

    fix: 스레드 풀 사이즈 조정

--- batch/src/main/java/com/yachaerang/batch/configuration/AsyncBatchConfig.java
+++ batch/src/main/java/com/yachaerang/batch/configuration/AsyncBatchConfig.java
@@ -12,9 +12,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @EnableAsync
 public class AsyncBatchConfig {
 
-    private static final int CORE_POOL_SIZE = 5;
+    private static final int CORE_POOL_SIZE = 10;
     private static final int MAX_POOL_SIZE = 20;
-    private static final int QUEUE_CAPACITY = 50;
+    private static final int QUEUE_CAPACITY = 100;
     private static final int KEEP_ALIVE_SEC = 30;
 
     @Bean
