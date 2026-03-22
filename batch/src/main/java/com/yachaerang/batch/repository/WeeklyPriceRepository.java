d70d22f17df36ed9be20c6b006743609af9b2ee2
yurry45 <yurry45@naver.com>
2026-03-23 01:23:17 +0900

    refactor: 집계함수 쿼리 삭제

--- batch/src/main/java/com/yachaerang/batch/repository/WeeklyPriceRepository.java
+++ batch/src/main/java/com/yachaerang/batch/repository/WeeklyPriceRepository.java
@@ -2,20 +2,11 @@ package com.yachaerang.batch.repository;
 
 import com.yachaerang.batch.domain.entity.WeeklyPrice;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Mapper
 public interface WeeklyPriceRepository {
-
-    /*
-    특정 일자에 대한 가격을 주간 데이터로 집계하기
-     */
-    List<WeeklyPrice> selectWeeklyAggregatedPrices(
-            @Param("startDate") LocalDate startDate,
-            @Param("endDate") LocalDate endDate);
 
     /*
     주간 가격 데이터 저장
